ab79196bb6e4ae14879c6e9867bd8ce16c432b43
Elle H <itsellej@gmail.com>
2018-09-17 13:32:39 +0100

    Start Study drill 2 for ex05

---
+++ ex05.rb
@@ -0,0 +1,29 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair"
+puts "His teeth are usually #{teeth}, depending on coffee"
+
+puts "If I add #{age}, #{height} and #{weight}, I get #{age + height + weight}"
+
+#Study Drills
+
+#1. Inches to cm converter
+
+cm_in_inch = 2.54
+
+inches = 10
+cm = 10
+
+puts "#{inches} inches converted to centimetres is #{inches * cm_in_inch} cm"
+
+puts "#{cm} cm converted to inches is #{cm / cm_in_inch} inches"
