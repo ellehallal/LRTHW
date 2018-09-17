b880afeb010874635b2a697816f8c6a5528701d6
Elle H <itsellej@gmail.com>
2018-09-17 14:29:45 +0100

    complete ex07.rb

---
+++ ex07.rb
@@ -0,0 +1,31 @@
+#puts a string
+puts "Mary had a little lamb"
+
+#puts a string including the string 'snow'
+puts "Its fleece as white as #{'snow'}"
+
+#puts a string
+puts "And everywhere that Mary went"
+
+#puts . 10 times
+puts "." * 10
+
+#12 variables each with a letter as a string as its value
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#print first 6 variables
+print end1 + end2 + end3 + end4 + end5 + end6
+
+#puts last 6 variables
+puts end7 + end8 + end9 + end10 + end11 + end12
