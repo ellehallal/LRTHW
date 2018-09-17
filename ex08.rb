834c8403209bbbd86a8be5d87cdaaa88b6c94d60
Elle H <itsellej@gmail.com>
2018-09-17 14:37:07 +0100

    Complete ex08.rb

---
+++ ex08.rb
@@ -0,0 +1,16 @@
+#formatter variable containing percentage literals
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+#setting values in the format of the formatter variable
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+#same as above with each on a new line
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
