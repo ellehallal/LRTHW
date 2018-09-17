271c0cf5f9a9cb30037d352984a9ed995eee98d1
Elle H <itsellej@gmail.com>
2018-09-17 14:48:42 +0100

    Complete ex09

---
+++ ex09.rb
@@ -0,0 +1,17 @@
+#assigning string with days to days variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#assigning string with months to months variable.New line between each month
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+#puts string with variable value
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+#puts a quote
+puts %q{
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
