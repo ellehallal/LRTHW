1df02cbdc332c194503b26c1d144256575a95611
Elle H <itsellej@gmail.com>
2018-09-17 14:02:49 +0100

    Complete ex06.rb

---
+++ ex06.rb
@@ -0,0 +1,50 @@
+#assigning integer 10 to variable
+types_of_people = 10
+
+#assigning a string to x which contains value of types_of_people variable
+x = "There are #{types_of_people} types of people"
+
+#string assigned to variables
+binary = "binary"
+do_not = "don't"
+
+#assigning a string to x which contains value of x and y variables
+y = "Those who know #{binary} and those who #{do_not}"
+
+#puts value of x and y variables
+puts x
+puts y
+
+#puts string which inclues value of x and y variables
+puts "I said: #{x}"
+puts "I said: #{y}"
+
+
+#assign boolean to hilarious variable
+hilarious = false
+
+#puts string which inclues value of hilarious variable
+joke_evaluation = "Isn't that joke so funny? #{hilarious}"
+
+#puts value of joke_evaluation variable
+puts joke_evaluation
+
+#assign strings to variables w and e
+w = "This is the left side of..."
+e = "a string with a right side"
+
+#puts value of w and e
+puts w + e
+
+
+#Study drills
+
+#1. String interpolation positions:
+ # - in variable x
+ # - in variable y (twice)
+ # - in strings on line 19 and 20
+ # - in variable joke_evaluation
+
+#2. Using w + e concatenates the string value in each variable
+
+#3. String interpolation doesn't appear to work with single quoted strings. Single-quoted strings puts string as is
