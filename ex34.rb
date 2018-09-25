2941b3f2a401ba74fa4a45b9c1a417ccf599a27c
Elle H <itsellej@gmail.com>
2018-09-25 18:16:16 +0100

    Complete ex34

---
+++ ex34.rb
@@ -0,0 +1,38 @@
+animals = ['bear', 'ruby', 'peacock', 'kangeroo', 'whale', 'platypus']
+
+
+
+
+#1 The animal at 1 is at 1 and is a ruby
+
+#2 The third (3rd) animal is at 2 and is peacock
+
+#3 The first (1st) animal is at 0 and is a bear
+
+#4 The animal at 3 is at 3 and is a kangeroo
+
+#5 The fifth (5th) animal is at 4 and is a whale
+
+#6 The animal at 2 is at 2 and is a peacock
+
+#7 The sixth (6th) animal is at 5 and is a platypus
+
+#8 The animal at 4 is at 4 and is a whale
+
+
+#Study drills
+
+#1. Because there is no year 0
+
+#2.
+
+my_array = ['cat', 'cake', 'confused', 'cup', 'cool', 'control']
+
+# The word at 1. - cake
+# The third (3rd) word. - confused at index 2
+# The first (1st) word. - cat at index 0
+# The word at 3 - cup
+# The fifth (5th) word - cool at index 4
+# The word at 2 - confused
+# The sixth (6th) word - control at index 5
+# The word at 4 - cup
